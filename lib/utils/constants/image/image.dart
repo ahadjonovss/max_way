3e211b08ad038a64c69e5d5b9ebf858b27a5a912
Asus <samandarahadjonov@gmail.com>
2023-04-05 15:13:15 +0500

    app constants done

---
+++ lib/utils/constants/image/image.dart
@@ -0,0 +1,30 @@
+class AppImages {
+  static const mainPath = 'assets/image';
+  static const menuPath = '$mainPath/menu';
+
+
+  //images of home page
+  static const burger = '${mainPath}buger.png';
+  static const combo = '${mainPath}combo.png';
+  static const logo = '${mainPath}logo.png';
+  static const pizza = '${mainPath}pizza.png';
+
+  //images of menu
+  static const cheeseBurger = '${menuPath}cheeseburger.png';
+  static const chilliBurger = '${menuPath}chilliburger.png';
+  static const cola = '${menuPath}cola.png';
+  static const combo1 = '${menuPath}combo1.png';
+  static const combo2 = '${menuPath}combo2.png';
+  static const combo3 = '${menuPath}combo3.png';
+  static const fanta = '${menuPath}fanta.png';
+  static const gavaya = '${menuPath}gavaya.png';
+  static const hamburger = '${menuPath}hamburger.png';
+  static const hotachchico = '${menuPath}hotachchico.png';
+  static const mexica = '${menuPath}mexica.png';
+  static const sprite = '${menuPath}sprite.png';
+
+
+
+}
+
+
